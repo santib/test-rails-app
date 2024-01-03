1476d9d6b1b6959bf4eebf2abaf619a23c8ea5d8
Santiago Bartesaghi <sbartesaghi@hotmail.com>
2024-01-03 18:19:47 -0300

    Change I/O for CPU intensive

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class PostTest < ActiveSupport::TestCase
   100.times do |i|
     test "the #{i} truth" do
-      sleep 1
+      (1000.years.ago.to_datetime..Time.current.to_date).sum { |date| date.to_i }
       assert true
     end
   end
